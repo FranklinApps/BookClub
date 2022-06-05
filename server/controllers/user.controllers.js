44d9675522bcfaf997b7b2a30dface0b612f9527
Frank <Franklinstevennealjr@gmail.com>
2022-06-04 21:10:50 -0600

    server update

--- server/controllers/user.controllers.js
+++ server/controllers/user.controllers.js
@@ -1,5 +1,4 @@
-const User = require('../models/user.model.js');
-
+const User = require('../models/user.models');
 
 module.exports ={
     createUser: (req,res) => {
--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const cors = require('cors')
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+require('./routes/user.routes')(app);
+require('./config/mongoose.config');
 const port=8000;
 
 app.listen(port, () => {console.log(`Listening on port ${port}`)});
