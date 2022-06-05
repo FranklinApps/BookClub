53602079f76458d815aa0d34de0b68bcf26fd2d8
Frank <Franklinstevennealjr@gmail.com>
2022-06-04 21:00:09 -0600

    server updates

--- server/server.js
+++ server/server.js
@@ -1,4 +1,9 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 const port=8000;
-console.log(`Listening on port ${port}`);
+
+app.listen(port, () => {console.log(`Listening on port ${port}`)});
